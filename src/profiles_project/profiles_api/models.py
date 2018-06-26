35670ba44076569e2c30a142f5e6a31b7dd3c335
Raymond Yu <raymondyu@raymonds-imac.attlocal.net>
2018-06-25 20:26:53 -0700

    Migrated models to database

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -3,30 +3,6 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
-class UserProfile(AbstractBaseUser, PermissionsMixin) :
-    # Represents a user profile inside our system
-    email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
-    is_active = models.Boolean(default=True)
-    is_staff = models.Boolean(default=False)
-
-    objects = UserProfileManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
-
-    def get_full_name(self) :
-        # Used to get a user's full name
-        return self.name
-
-    def get_short_name(self) :
-        # Used to get a user's short name
-        return self.name
-
-    def __str__(self) :
-        # Django uses this when it needs to convert the object to a string
-        return self.email
-
 class UserProfileManager(BaseUserManager) :
     # Helps Django work with our custom user model
 
@@ -54,8 +30,26 @@ class UserProfileManager(BaseUserManager) :
 
         return user
 
+class UserProfile(AbstractBaseUser, PermissionsMixin) :
+    # Represents a user profile inside our system
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
+    objects = UserProfileManager()
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
 
+    def get_full_name(self) :
+        # Used to get a user's full name
+        return self.name
 
-# Create your models here.
+    def get_short_name(self) :
+        # Used to get a user's short name
+        return self.name
+
+    def __str__(self) :
+        # Django uses this when it needs to convert the object to a string
+        return self.email
