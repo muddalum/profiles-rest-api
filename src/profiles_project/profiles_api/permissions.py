e6ca1c4ca57e65451c26671a78d286e9d33df07f
playbowartist <muddalum@hotmail.com>
2018-06-27 23:32:48 -0700

    Created User Profile Feed

--- src/profiles_project/profiles_api/permissions.py
+++ src/profiles_project/profiles_api/permissions.py
@@ -12,3 +12,16 @@ class UpdateOwnProfile(permissions.BasePermission) :
         # Else user is requesting a PUT/POST/DELETE, so need to check if current user is logged in and authorized to change the data it is trying to manipulate
         # Return whether the object's ID is the same as the current user's ID
         return obj.id == request.user.id
+
+class PostOwnStatus(permissions.BasePermission) :
+    # Allow users to update their own status
+
+    def has_object_permission(self, request, view, obj) :
+        # Checks the user is trying to update their own status
+        # Users can view other ppl's status, but can't update them
+
+        if request.method in permissions.SAFE_METHODS :
+            return True
+
+        return obj.user_profile.id == request.user.id
+        
